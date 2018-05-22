829e57feba048aa6dfc79912eb6e5832e178c113
k0nkupa <theton369@gmail.com>
2018-05-22 22:27:23 +1200

    Update convo.js

--- public/javascripts/convo.js
+++ public/javascripts/convo.js
@@ -1,7 +1,6 @@
 var botui = new BotUI('api-bot');
 
-var port = process.env.PORT;
-var socket = io.connect('https://software-engineering-chatbot1.herokuapp.com:port');
+var socket = io.connect('http://localhost:8010');
 
 socket.removeAllListeners();
 botui.message.add({
